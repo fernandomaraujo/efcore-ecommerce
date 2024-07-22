1ea3b823b1751149439988d5f07a216192d16d17
fernandomaraujo <fernandomarinho.araujo@gmail.com>
2024-07-22 11:46:33 -0400

    many-to-many: configuração inicial do eCommerceOfficeContext

--- eCommerce.Office/eCommerceOfficeContext.cs
+++ eCommerce.Office/eCommerceOfficeContext.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using eCommerce.Office.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerce.Office
 {
-    internal class eCommerceOfficeContext
+    public class eCommerceOfficeContext : DbContext
     {
+        public DbSet<Colaborador>? Colaboradores { get; set; }
+        public DbSet<Setor>? Setores { get; set; }
+        public DbSet<Turma>? Turmas { get; set; }
+        public DbSet<Veiculo>? Veiculos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            /**
+             * Não é recomendável deixar uma connection string exposta assim.
+             * Porém este projeto é apenas pra fins de estudos no Entity Framework.
+             * 
+             */
+            optionsBuilder.UseSqlServer(
+                "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerceOffice;Integrated Security=True;"
+            );
+        }
+
     }
 }
