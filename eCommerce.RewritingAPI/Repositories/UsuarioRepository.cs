7cafcf0f27140a8d7852b89a166a667c0d7aa766
fernandomaraujo <fernandomarinho.araujo@gmail.com>
2024-08-15 12:31:03 -0400

    Recriando projeto de API para novo capítulo

---
+++ eCommerce.RewritingAPI/Repositories/UsuarioRepository.cs
@@ -0,0 +1,45 @@
+﻿using eCommerce.Models;
+using eCommerce.RewritingAPI.Database;
+
+namespace eCommerce.RewritingAPI.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly eCommerceContext _db;
+        public UsuarioRepository(eCommerceContext db)
+        {
+            _db = db;
+        }
+
+        public List<Usuario> Get()
+        {
+            return _db.Usuarios.OrderBy(a => a.Id).ToList();
+        }
+
+        public Usuario Get(int id)
+        {
+            return _db.Usuarios.Find(id)!;
+        }
+
+        public void Add(Usuario usuario)
+        {
+            /*
+             * Unit of Works
+             */
+            _db.Usuarios.Add(usuario);
+            _db.SaveChanges();
+        }
+
+        public void Update(Usuario usuario)
+        {
+            _db.Usuarios.Update(usuario);
+            _db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            _db.Usuarios.Remove(Get(id));
+            _db.SaveChanges();
+        }
+    }
+}
