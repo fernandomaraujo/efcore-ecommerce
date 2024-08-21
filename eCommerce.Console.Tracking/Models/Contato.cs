36ae1b7d4775a12d89cbfb1ee7afaa9a0a320a26
fernandomaraujo <fernandomarinho.araujo@gmail.com>
2024-08-21 11:13:01 -0400

    Change Tracker: criando projeto

---
+++ eCommerce.Console.Tracking/Models/Contato.cs
@@ -0,0 +1,11 @@
+﻿namespace eCommerce.Console.Tracking
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public string? Telefone { get; set; }
+        public string? Celular { get; set; }
+        public Usuario? Usuario { get; set; }
+    }
+}
---
+++ eCommerce.Console.Tracking/eCommerceContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Console.Tracking
+{
+    public class eCommerceContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;")
+                //.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+                //.EnableSensitiveDataLogging()
+                //.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+                ;
+
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Contato> Contatos { get; set; }
+        public DbSet<EnderecoEntrega> EnderecosEntrega { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+    }
+}
