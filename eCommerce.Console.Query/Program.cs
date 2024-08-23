a175ff2fcd03ea112e7a463827f43974899c5753
fernandomaraujo <fernandomarinho.araujo@gmail.com>
2024-08-23 15:18:55 -0400

    Criando projeto + Sobre consultas com ToList, ToArray, ToDictionary e ToHashSet

---
+++ eCommerce.Console.Query/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using eCommerce.Console.Query;
+
+var db = new eCommerceContext();
+var usuarios = db.Usuarios!.ToList();
+
+// Sobre consultas com ToList, ToArray, ToDictionary e ToHashSet
+
+/*
+    db.Usuarios!.{Methods > LING > EF > SQL > SGBD}.ToList().{Methods > LINQ > C# > Processador+Memória}
+ 
+ */
+
+Console.WriteLine("Listagem de usuários:");
+foreach (var usuario in usuarios)
+{
+    Console.WriteLine($" - {usuario.Nome}");
+}
---
+++ eCommerce.Console.Query/eCommerceContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Console.Query
+{
+    public class eCommerceContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;");
+
+        }
+
+        public DbSet<Usuario>? Usuarios { get; set; }
+        public DbSet<Contato>? Contatos { get; set; }
+        public DbSet<EnderecoEntrega>? EnderecosEntrega { get; set; }
+        public DbSet<Departamento>? Departamentos { get; set; }
+    }
+}
