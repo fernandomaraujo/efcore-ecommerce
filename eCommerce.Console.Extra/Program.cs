9ebbbf39c53fae6f1ab2f341e70134a8f77b84cf
fernandomaraujo <fernandomarinho.araujo@gmail.com>
2024-08-26 16:18:21 -0400

    Tabelas temporais: inserindo dados no histórico

--- eCommerce.Console.Extra/Program.cs
+++ eCommerce.Console.Extra/Program.cs
@@ -1,4 +1,5 @@
-﻿using eCommerce.Console.Query;
+﻿using eCommerce.Console.Extra.Models;
+using eCommerce.Console.Query;
 using Microsoft.EntityFrameworkCore;
 
 var db = new eCommerceContext();
@@ -23,3 +24,34 @@ var usuariosList03 = db.Usuarios!.ToList();
  - Update-Database
  - Tabela temporária criada
  */
+
+// Inserindo dados no histórico
+
+var novoUsuario = new Usuario
+{
+    Nome = "Mahomes",
+    Email = "mahomes@chiefs.com",
+    Sexo = "M",
+    CPF = "123.456.789-10",
+    NomeMae = "Randi Martin",
+    SituacaoCadastro = SituacaoCadastro.Ativo,
+    DataCadastro = DateTimeOffset.Now,
+};
+
+db.Add(novoUsuario);
+db.SaveChanges();
+
+// Procura pelo mesmo usuário, o atualizando
+
+// Assumindo que o Id do usuário cadastrado anteriormente é igual à 2
+var usuarioAtualizado = db.Usuarios!.Find(2);
+usuarioAtualizado!.Nome = "Patrick Mahomes";
+db.SaveChanges();
+
+/* 
+ Agora a tabela de histórico registra a primeira versão do usuário, como ele
+ era antes da atualização. Registra também o período em que aquela informação 
+ permaneceu no banco (nas colunas PeriodoInicial e PeriodoFinal).
+
+   Na tabela usuário estará atualizado.
+*/
