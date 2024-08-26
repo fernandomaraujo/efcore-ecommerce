8aba731b6f03f7961fea6dec22f8742e853ff0af
fernandomaraujo <fernandomarinho.araujo@gmail.com>
2024-08-26 15:27:13 -0400

    Global Filter

---
+++ eCommerce.Console.Extra/Program.cs
@@ -0,0 +1,10 @@
+﻿using eCommerce.Console.Query;
+using Microsoft.EntityFrameworkCore;
+
+var db = new eCommerceContext();
+
+// Como um filtro global foi aplicado no OnModelCreating, apenas usuários ativos serão trazidos do banco de dados
+var usuariosList = db.Usuarios!.ToList();
+
+// Para ignorar o filtro global
+var usuariosList02 = db.Usuarios!.IgnoreQueryFilters().ToList();
---
+++ eCommerce.Console.Extra/eCommerceContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using eCommerce.Console.Extra.Models;
+
+namespace eCommerce.Console.Query
+{
+    public class eCommerceContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(
+                    "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;");
+        }
+
+        public DbSet<Usuario>? Usuarios { get; set; }
+        public DbSet<Contato>? Contatos { get; set; }
+        public DbSet<EnderecoEntrega>? EnderecosEntrega { get; set; }
+        public DbSet<Departamento>? Departamentos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Filtro global para trazer apenas usuários ativos
+            modelBuilder.Entity<Usuario>()
+                .HasQueryFilter(a => a.SituacaoCadastro == "A");
+
+        }
+    }
+}
